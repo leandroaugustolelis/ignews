b252a04f4073f093c390cdeb17d048bbe6ee7539
Leandro Lelis <leandroaugustolelis@gmail.com>
2023-03-21 14:07:37 -0300

    creating components Header, SignInButton, SubscribeButton and styles

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,20 @@
+import { FaGithub } from "react-icons/fa";
+import styles from "./styles.module.scss";
+import { FiX } from "react-icons/fi";
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#04d361" />
+      Leandro Lelis
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
-import styles from "../styles/home.module.scss";
+import { SubscribeButton } from "@/components/SubscribeButton";
+import Head from "next/head";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news </title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span> React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }
